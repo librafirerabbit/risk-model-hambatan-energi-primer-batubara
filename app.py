37884eb48f0161eca6c9f25cf88c71aeddf143f4
librafirerabbit <librafirerabbit@gmail.com>
2026-09-14 16:16:10 +0700

    Update

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.set_page_config(
     page_title="Risk Model Hambatan Energi Primer Batubara",
     page_icon="⚡",
     layout="wide",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="collapsed",
 )
 
 
